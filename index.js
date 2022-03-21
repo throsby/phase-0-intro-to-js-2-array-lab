f14497af75ae5f4298637fc2e27cc775d8e363dd
Throsby Wells <throsbywells@gmail.com>
2022-03-21 12:16:04 -0400

    commit tree 88ec8f62855938c068e785d1ab692656662572cf

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.splice(-1,1)
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.splice(0,1)
+}
+
+function appendCat(name) {
+    return cats.concat(name)
+}
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+
+function removeLastCat(){
+    return cats.slice(0,-1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1,cats.length)
+}
